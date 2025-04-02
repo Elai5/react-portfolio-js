1b2bb4bae4e3cf498512259c6296f5b0c5a9bb87
Elai5 <yvettelaine007@gmail.com>
2025-04-02 16:33:59 +0300

    added about-text.fullu responsive:

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,6 +14,29 @@ const About = () => {
             className="rounded-full shadow-xl w-56 h-56 object-cover"
           />
         </div>
+
+        <div className="text-center md:text-left">
+          <h2 className="text-4xl font-bold text-blue-500 mb-4">About Me</h2>
+          <p className="text-lg text-gray-300 mb-6">
+            Hello, I'm Elaine Yvette, a passionate Front-end Developer with a
+            keen intrest in UI/UX Design and web animation. I specialize in
+            creating websites that provide seamless user experience. With a love
+            for creative problem-solving, I am to bring innovative
+            digitalsolutions to life.
+          </p>
+          <p className="text-lg text-gray-300  mb-6">
+            Throughout my career, I've worked on several projects where I've
+            contributed my skills web automation,React,Airtable and many more.
+            I'm constantly exploring new technologies and methodologies to stay
+            ahead in this rapidily evolving field.
+          </p>
+          <a
+            href="#projects"
+            className="mt-6 inline-block px-8 py-3 bg-blue-500 text-white rounded-lg text-xl shadow-lg hover:bg-blue-600 transition duration-300"
+          >
+            View My Work
+          </a>
+        </div>
       </div>
     </div>
   );
