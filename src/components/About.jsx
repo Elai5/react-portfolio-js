46d4ec7a44bb99fd68944823695d83a5fd7bf196
Elai5 <yvettelaine007@gmail.com>
2025-04-06 15:57:38 +0300

    small text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
           </div>
           <div className="w-full md:w-1/2 md:pl-10">
             <h2 className="text-3xl font-bold mb-4 text-blue-500 ">About Me</h2>
-            <h3 className="text-xl text-gray-600 mb-5 pl-8 italic">
+            <h3 className="text-sm md:text-xl text-gray-600 mb-5 pl-8 italic">
               {" "}
               "A passionate developer, lifelong learner, and mentor."
             </h3>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects() {
       className="py-20 px-6 pl-15 pr-15 bg-gray-950 text-blue-500"
     >
       <h2 className="text-4xl font-bold text-center mb-6">My Projects</h2>
-      <h3 className="text-gray-600 text-center mb-8 text-xl italic">
+      <h3 className="text-gray-600 text-center mb-8 text-sm md:text-xl italic">
         "Each project begins with a problem, and I craft solution by translating
         ideas into powerful, intutive design".{" "}
       </h3>
@@ -30,7 +30,9 @@ export default function Projects() {
             />
             <div className="p-6 rounded-lg shadow-lg transition-transform duration--300 ease-in-out hover::scale-105 hover:shadow-2xl">
               <h3 className="text-2xl font-semibold mb-2">{project.title}</h3>
-              <p className="text-gray-300 mb-4 text-sm">{project.description}</p>
+              <p className="text-gray-300 mb-4 text-sm">
+                {project.description}
+              </p>
               <div className="flex flex-wrap gap-2 mb-4">
                 {project.tech.map((tech, idx) => (
                   <span
