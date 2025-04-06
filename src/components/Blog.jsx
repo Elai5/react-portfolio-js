3637e1aede6b2e25b5612c85334886b4aa813baf
Elai5 <yvettelaine007@gmail.com>
2025-04-06 16:03:18 +0300

    small text on blog for phone

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,11 +7,11 @@ const Blog = () => {
   return (
     <section id="blog" className="py-12 bg-gray-900">
       <div className="max-w-screen-xl mx-auto px-4">
-        <h2 className="text-3xl font-semibold text-center text-blue-500 mb-5">
+        <h2 className="text-2xl md:text-3xl font-semibold text-center text-blue-500 mb-5">
           {" "}
           My Journey
         </h2>
-        <h3 className="text-xl text-center text-gray-600 italic mb-6">
+        <h3 className="text-base md:text-xl text-center text-gray-600 italic mb-6">
           "A path shaped by passion, perseverance, and continuous learning."
         </h3>
         <div className="relative">
@@ -21,13 +21,15 @@ const Blog = () => {
               <div key={post.id} className="mb-8 relative">
                 <div className="absolute left-0 transform -translate-x-1/2 w-4 h-4 bg-blue-500 rounded-full border-4 border-white " />
                 <div className="ml-12 pl-8 pr-4 bg-white p-6 rounded-lg shadow-lg">
-                  <h3 className="text-xl font-semibold text-gray-800">
+                  <h3 className=" text-lg md:text-xl font-semibold text-gray-800">
                     {post.title}
                   </h3>
-                  <p className="text-sm text-gray-500">
+                  <p className="text-xs md:text-sm text-gray-500">
                     {post.timeline} | {post.community}
                   </p>
-                  <p className="text-gray-700 mt-4">{post.description}</p>
+                  <p className="text-sm md:text-gray-700 mt-4">
+                    {post.description}
+                  </p>
                 </div>
               </div>
             ))}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,10 +9,11 @@ export default function Projects() {
       className="py-20 px-6 pl-15 pr-15 bg-gray-950 text-blue-500"
     >
       <h2 className="text-4xl font-bold text-center mb-6">My Projects</h2>
-      <h3 className="text-gray-600 text-center mb-8 text-sm md:text-xl italic">
-        "Each project begins with a problem, and I craft solution by translating
-        ideas into powerful, intutive design".{" "}
+      <h3 className="text-gray-600 text-center mb-8 text-xs md:text-xl italic">
+        "Each project begins with a problem, and I craft solutions by
+        translating ideas into powerful, intuitive design."
       </h3>
+
       <div className="grid gap-6 md:grid-cols-2">
         {projects.map((project, index) => (
           <motion.div
